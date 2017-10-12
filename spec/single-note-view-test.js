f3805af3a65e1618d5b2c00999e31578e8d25722
Nero Siva <neroshan12@gmail.com>
2017-10-12 17:30:48 +0100

    Add single note view and rearrange files

---
+++ spec/single-note-view-test.js
@@ -0,0 +1,11 @@
+(function(){
+  var noteDouble = {
+    getText: function() {
+      return "Favourite drink: seltzer";
+    }
+  };
+
+  var singleNoteView = new SingleNoteView(noteDouble);
+  var testText2 = "<div>Favourite drink: seltzer</div>";
+  assert.isTrue(singleNoteView.getHTML() === testText2);
+})();
