8835d6d5af1a6a70f593c9d8c5297fe53d826bee
Nero Siva <neroshan12@gmail.com>
2017-10-13 16:37:20 +0100

    Add notes

--- src/note-list-model.js
+++ src/note-list-model.js
@@ -2,9 +2,9 @@
 function NoteList() {
   this.lists = [];
 }
-NoteList.prototype.createAndSave = function(text, NoteConstructor = Note) {
-  var note = new NoteConstructor(text);
-  this.lists.push(note);
+NoteList.prototype.createAndSave = function(text, NoteConstructor = Note) {  // purpose of this is that you  pass it a text and something else. if you dont pass it a something else then it will default to note
+  var note = new NoteConstructor(text); // name of noteconstructor is irrelevant its just a placeholder, this method does not care what it is passed as long as it is a note (lowercase)
+  this.lists.push(note);  // if text and nothing else is passed on line 5 then the default value will be note. if something else is passed in then it will just use that to create the note 
 };
 
 NoteList.prototype.viewNoteList = function() {
